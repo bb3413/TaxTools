89349c33b3b4960febfec5aa14875e66d836d993
Bruce Blinn <bruce_blinn@yahoo.com>
2026-07-21 19:15:43 -0700

    Update the version number. The OO version is live in the develop branch.

--- Version/Version.js
+++ Version/Version.js
@@ -13,7 +13,7 @@
  * number a clickable link so that when it is clicked, the version log is displayed.
  */
 
-const tax_tools_version		= "2025.15.003@";
+const tax_tools_version		= "2025.15.004@";
 
 function Initialize() {
 	// Find the version number by its ID.
