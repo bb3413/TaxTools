a93aacd40dc891be014ae6738967c25121d054e0
Bruce Blinn <bruce_blinn@yahoo.com>
2026-08-30 10:33:54 -0700

    Update version information.

--- Version/Version.js
+++ Version/Version.js
@@ -13,7 +13,7 @@
  * number a clickable link so that when it is clicked, the version log is displayed.
  */
 
-const tax_tools_version		= "2025.15.017@";
+const tax_tools_version		= "2025.15.018@";
 
 function Initialize() {
 	// Find the version number by its ID.
